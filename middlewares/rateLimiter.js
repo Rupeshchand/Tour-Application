1b09630f5ccd424bc454ae39b3e14ce0f2f56d5c
Rupesh Chand <rupeshchandvasantam@gmail.com>
2025-04-23 12:19:09 +0530

    added multer package to upload files

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit"
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 20,
+    max: 5,
     message: "Too many requests try again after some time",
     headers : true
 })
